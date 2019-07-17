41a3b908ac3aea1dad54f657221e0e15bf24a926
Zhi En Dylan Quek <s10186935@connect.np.edu.sg>
2019-07-18 00:16:34 +0800

    test

---
+++ app/src/main/java/com/dylan/geogrammad/MainActivity.java
@@ -0,0 +1,44 @@
+package com.dylan.geogrammad;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navbar = findViewById(R.id.bottomNavBar);
+        navbar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.friendsNav:
+                        loadFragment(new FriendsFragment());
+                        return true;
+                    case R.id.cameraNav:
+                        loadFragment(new CameraFragment());
+                        return true;
+                    case R.id.accountNav:
+                        loadFragment(new AccountFragment());
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
