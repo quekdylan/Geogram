4a340bc805d904d4db6a1f087002d9046d7db800
Dong Han Kua <s10189943@connect.np.edu.sg>
2019-07-30 14:24:30 +0800

    OwO

--- app/src/main/java/com/dylan/geogrammad/GlideModuleAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.dylan.geogrammad;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class GlideModuleAdapter extends AppGlideModule {
-
-}
--- app/src/main/java/com/dylan/geogrammad/InfoWindowAdapter.java
+++ app/src/main/java/com/dylan/geogrammad/InfoWindowAdapter.java
@@ -2,19 +2,30 @@ package com.dylan.geogrammad;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
 
+
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.io.InputStream;
+import java.util.List;
+
 
 public class InfoWindowAdapter implements GoogleMap.InfoWindowAdapter{
 
@@ -36,18 +47,24 @@ public class InfoWindowAdapter implements GoogleMap.InfoWindowAdapter{
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v =  inflater.inflate(R.layout.gir_forest, null);
 
-// Reference to an image file in Cloud Storage
-        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child(marker.getSnippet()).child("Images");
+        String username = marker.getSnippet();
+        ImageView imageView1 = (ImageView) v.findViewById(R.id.imageView1);
 
-// ImageView in your Activity
-        ImageView imageView = v.findViewById(R.id.imageView);
 
-// Download directly from StorageReference using Glide
-// (See MyAppGlideModule for Loader registration)
-        Glide.with(v.findViewById(R.id.imageView))
-                .load(storageReference)
-                .into(imageView);
 
+        // Reference to an image file in Cloud Storage
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+
+        StorageReference pathReference = storageRef.child("users/thanks/Images/40761221-e0b0-42b7-8308-824136016cba");
+
+
+        ImageView image = (ImageView)v.findViewById(R.id.imageView1);
+
+// Load the image using Glide
+        Glide.with(v.getContext())
+                .using(new FirebaseImageLoader())
+                .load(pathReference)
+                .into(image);
 
         TextView tvGir = (TextView) v.findViewById(R.id.tvgir);
         TextView tvDetails = (TextView) v.findViewById(R.id.tvd);
